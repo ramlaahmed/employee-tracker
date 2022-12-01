336b233f9ead6e339fdd24d06044df945627c60c
ramlaahmed <ramlaahmed01@gmail.com>
2022-12-01 14:19:32 +0000

    installed console.table and console-table-printer

--- db/seed.sql
+++ db/seed.sql
@@ -1,26 +1,30 @@
-INSERT INTO departments (name)
+
+INSERT INTO department (name)
 VALUES
-('Management'),
-('Accounting'),
-('Marketing'),
-('Web Development');
+("Management"),
+("Accounting"),
+("Marketing"),
+("Web Development")
+("legal");
 
 
-INSERT INTO roles (title, id, salary)
+INSERT INTO role (title, department_id, salary)
 VALUES
-('CEO', 1, 8000),
-('Manager', 1, 45000),
-('Sales Representative', 2, 2300),
-('Customer Service Agent', 3, 31000),
-('Full Stack Web Developer', 4 , 2300);
+("CEO", 1, 8000),
+("Manager", 1, 45000),
+("Sales Representative", 2, 2300),
+("Customer Service Agent", 3, 31000),
+("Full Stack Web Developer", 4 , 2300);
+("Projext Manager",);
+("Lawyer",);
 
 
-INSERT INTO employees (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Jessica', 'Harris', 1, NULL),
-('Mandy', 'Beck', 2, 1), 
-('Aand', 'Wes', 2, 1),
-('Jodie', 'Jades', 3, 2),
-('Rick', 'Smith', 3, 3),
-('luke', 'James', 4, 1),
-('Andrea', 'Quincy', 3, 2);
+("Jessica", "Harris", 1, NULL),
+("Mandy", "Beck", 2, 1), 
+("Aand", "Wes", 2, NULL),
+("Jodie", "Jades", 3, 2),
+("Rick", "Smith", 3, 3),
+("luke", "James", 4, 1),
+("Andrea", "Quincy", 3, 2);
