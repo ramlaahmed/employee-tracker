54da85752040de7fccd40ab18fe39d36453a7a6c
ramlaahmed <ramlaahmed01@gmail.com>
2022-12-01 15:18:06 +0000

    fixed the database

--- db/schema.sql
+++ db/schema.sql
@@ -1,36 +1,26 @@
-DROP DATABASE IF EXISTS employee_db;
-CREATE DATABASE employee_db;
-USE employee_db; 
-
-
-
-
+DROP DATABASE IF EXISTS employee_DB;
+CREATE DATABASE employee_DB;
+USE employee_DB;
 CREATE TABLE department (
-  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(30) UNIQUE NOT NULL
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
-
-
 CREATE TABLE role (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(12,2) NOT NULL,
     department_id INT,
-   INDEX dep_ind (department_id),
-CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
+    PRIMARY KEY (id),
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
 );
-
-
 CREATE TABLE employee (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT,
-    INDEX role_ind (role_id),
-  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
-  manager_id INT UNSIGNED,
-  INDEX man_ind (manager_id),
-  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+    role_id INTEGER,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
-    
-    
--- db/seed.sql
+++ db/seed.sql
@@ -1,31 +1,29 @@
- USE employee_db; 
-
 INSERT INTO department (name)
 VALUES
-('Management'),
-('Accounting'),
-('Marketing'),
-('Web Development'),
-('legal');
+("Management"),
+("Accounting"),
+("Marketing"),
+("Web Development"),
+("legal");
 
 
 INSERT INTO role (title, department_id, salary)
 VALUES
-('CEO', 1, 8000),
-('Manager', 1, 45000),
-('Sales Representative', 2, 2300),
-('Customer Service Agent', 3, 31000),
-('Full Stack Web Developer', 4 , 2300),
-('Projext Manager',5,6000),
-('Lawyer',6,8000);
+("CEO", 1, 8000),
+("Manager", 1, 45000),
+("Sales Representative", 2, 2300),
+("Customer Service Agent", 3, 31000),
+("Full Stack Web Developer", 4 , 2300),
+("Projext Manager",4, 30000),
+("Lawyer",5, 45000);
 
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-('Jessica', 'Harris', 1, NULL),
-('Mandy', 'Beck', 2, 1), 
-('Aand', 'Wes', 2, NULL),
-('Jodie', 'Jades', 3, 2),
-('Rick', 'Smith', 3, 3),
-('luke', 'James', 4, 1),
-('Andrea', 'Quincy', 3, 2);
+("Jessica", "Harris", 1, NULL),
+("Mandy", "Beck", 2, 1), 
+("Aand", "Wes", 2, NULL),
+("Jodie", "Jades", 3, 2),
+("Rick", "Smith", 3, 3),
+("luke", "James", 4, 1),
+("Andrea", "Quincy", 3, 2);
